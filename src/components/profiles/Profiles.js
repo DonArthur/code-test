d4c343f8fc38548cd2b6aeaf4b6bbae77626f747
geraldy.miero <geraldy.miero@maucash.id>
2025-04-19 19:43:24 +0700

    update: add modal

--- src/components/profiles/Profiles.js
+++ src/components/profiles/Profiles.js
@@ -1,14 +1,7 @@
-import Image from 'next/image'
 import styles from './Profiles.module.css'
 import ProfileCard from '../profile-card/ProfileCard'
 
 const Profiles = ({ data }) => {
-  const formatShortNumber = (num) => {
-    if (num >= 1000000) return (num / 1000000).toFixed(1).replace(/\.0$/, '') + 'M'
-    if (num >= 1000) return (num / 1000).toFixed(1).replace(/\.0$/, '') + 'K'
-    return num.toString()
-  }
-
   return (
     <div className={styles.wrapper}>
       <h2 className={styles.title}>Profiles</h2>
